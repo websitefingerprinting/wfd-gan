6457e88a192f6cc56c22892fd3eff1f9f3568b72
龚佳俊 <jgongac@connect.ust.hk>
2020-12-21 16:24:06 +0800

    bug fix

--- src/glue_script.py
+++ src/glue_script.py
@@ -8,8 +8,10 @@ src_path = join(BASE_DIR, 'src/gan_glue.py')
 for m in range(2,17):
     n = 9900 // m
     # print("m :{}, n: {}".format(m, n))
-    cmd = "python3 " + src_path + " ~/websitefingerprinting/data/evaluation/ -n "+ str(n)+" -m "+str(m)+ " -b 10 -noise True -mode fix"
-    # print(cmd)
-    subprocess.call(cmd, shell = True)
+    cmd = "python3 " + src_path + " --dir ~/websitefingerprinting/data/evaluation/ " + \
+          "--model dump/evaluation/model_/ --ipt dump/evaluation/kde/cdf.npy " + \
+          "-n "+ str(n)+" -m "+str(m)+ " -b 10 --noise True --mode fix"
+    print(cmd)
+    # subprocess.call(cmd, shell = True)
 
 
