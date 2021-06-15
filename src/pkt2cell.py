86dc3adad323bf65613efc1e478d40f3d4e98c28
Jiajun GONG <jgongac@connect.ust.hk>
2021-06-15 17:05:07 +0800

    add pkt2cell function/ remove some deprecated functions.

--- src/pkt2cell.py
+++ src/pkt2cell.py
@@ -1,16 +1,17 @@
 import numpy as np
 import os
+from os.path import join
 import common as cm
 import utils
 import argparse
 import pandas as pd
 import multiprocessing as mp
 
-logger = utils.init_logger('burst')
-
+logger = utils.init_logger('wfd')
+CELL_SIZE = 514
 
 def parse_arguments():
-    parser = argparse.ArgumentParser(description='Generat a distribution for ipt')
+    parser = argparse.ArgumentParser(description='Parse cells from pkt files (only implemented the closed world)')
 
     parser.add_argument('--dir',
                         metavar='<traces path>',
@@ -18,12 +19,12 @@ def parse_arguments():
                         help='Path to the directory with the traffic traces to be simulated.')
     parser.add_argument('--format',
                         metavar='<file suffix>',
-                        default=".cell",
+                        default=".pkt",
                         )
     parser.add_argument('--proc_num',
                         type=int,
                         metavar='<process num>',
-                        default=2,
+                        default=60,
                         help='The num of parallel workers')
 
     # Parse arguments
@@ -32,36 +33,26 @@ def parse_arguments():
 
 
 def parse(fpath):
-    global format
+    global format, dst_dir
     with open(fpath, "r") as f:
         tmp = f.readlines()
-    t = pd.Series(tmp).str.slice(0, -1).str.split("\t", expand=True).astype(np.int64)
-    trace = np.array(t)
-    trace[:, 0] = (trace[:,0] - trace[0, 0])
-    trace = trace.astype(float)
-    trace[:,0] /= 1e9
-    burst = []
-    cursign = np.sign(trace[0, 1])
-    curburst = 0
-    curtime = trace[0, 0]
-    for pkt in trace:
-        if np.sign(pkt[1]) == cursign:
-            curburst += abs(pkt[1])
-        else:
-            burst.append([curtime, curburst * cursign])
-            curtime = pkt[0]
-            curburst = abs(pkt[1])
-            cursign = np.sign(pkt[1])
-    burst.append([curtime, curburst * cursign])
-
-    burst_outputdir = fpath.split(format)[0] + '.burst'
-    with open(burst_outputdir, 'w') as f:
-        for b in burst:
-            f.write('{:.4f}\t{:.0f}\n'.format(b[0], b[1]))
+    t = pd.Series(tmp).str.slice(0, -1).str.split("\t", expand=True).astype(float)
+    pkts = np.array(t)
+    cells = []
+    fname = fpath.split('/')[-1]
+    dst_fname = fname.replace(format, '.cell')
+    with open(join(dst_dir, dst_fname), 'w') as f:
+        for pkt in pkts:
+            cur_t = pkt[0]
+            size = pkt[1]
+            cur_sign = np.sign(size)
+            num_of_cells = int(abs(np.round(size / CELL_SIZE)))
+            for _ in range(num_of_cells):
+                f.write('{:.6f}\t{:.0f}\n'.format(cur_t, cur_sign))
 
 
 if __name__ == '__main__':
-    global MON_SITE_NUM, length, format
+    global format, dst_dir
     # parser config and arguments
     args = parse_arguments()
     format = args.format
@@ -70,21 +61,16 @@ if __name__ == '__main__':
     cf = utils.read_conf(cm.confdir)
     MON_SITE_NUM = int(cf['monitored_site_num'])
     MON_INST_NUM = int(cf['monitored_inst_num'])
-    if cf['open_world'] == '1':
-        UNMON_SITE_NUM = int(cf['unmonitored_site_num'])
-        OPEN_WORLD = 1
-    else:
-        OPEN_WORLD = 0
-        UNMON_SITE_NUM = 0
 
     flist = []
     for i in range(MON_SITE_NUM):
         for j in range(MON_INST_NUM):
             if os.path.exists(os.path.join(args.dir, str(i) + "-" + str(j) + args.format)):
                 flist.append(os.path.join(args.dir, str(i) + "-" + str(j) + args.format))
-    for i in range(UNMON_SITE_NUM):
-        if os.path.exists(os.path.join(args.dir, str(i) + args.format)):
-            flist.append(os.path.join(args.dir, str(i) + args.format))
+
+    dst_dir = args.dir.rstrip('/') + '_cell'
+    if not os.path.exists(dst_dir):
+        os.makedirs(dst_dir)
 
     with mp.Pool(processes=args.proc_num) as p:
         p.map(parse, flist)
