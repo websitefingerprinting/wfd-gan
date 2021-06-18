3e7c976133b3019fb0a1854396cb04e286f96aaf
Jiajun GONG <jgongac@connect.ust.hk>
2021-06-18 16:16:10 +0800

    resampling use kernel

--- src/ipt_sampler.py
+++ src/ipt_sampler.py
@@ -8,9 +8,9 @@ import utils
 import argparse
 import pandas as pd
 from KDEpy import FFTKDE
+from KDEpy.bw_selection import silvermans_rule, improved_sheather_jones
 
 logger = utils.init_logger('ipt')
-
 
 
 def parse_arguments():
@@ -48,7 +48,7 @@ def parse(fpath):
         if cur_sign == last_sign:
             continue
         else:
-            filtered_trace.append([time,size])
+            filtered_trace.append([time, size])
             last_sign = cur_sign
     filtered_trace = np.array(filtered_trace)
     outgoing_filtered_trace = filtered_trace[filtered_trace[:, 1] > 0]
@@ -60,14 +60,12 @@ def parse(fpath):
         cur_sign = np.sign(cur_size)
         if cur_sign > 0:
             continue
-        last_time = filtered_trace[i-1][0]
+        last_time = filtered_trace[i - 1][0]
         o2i.append(cur_time - last_time)
     o2i = np.array(o2i)
     assert (o2i >= 0).all()
     return list(o2o), list(o2i)
 
-    
-        
 
 def prepare_dataset(flist):
     with multiprocessing.Pool(2) as p:
@@ -119,8 +117,25 @@ if __name__ == '__main__':
     logger.info("KDE modeling...")
     pdf = {}
     for key in ipt_dataset.keys():
-        log_ipt = np.log10(ipt_dataset[key])
+        data = ipt_dataset[key]
+        data[data == 0] = 1e-9
+        log_ipt = np.log10(data)
         x, y = FFTKDE(kernel='gaussian', bw='ISJ').fit(log_ipt).evaluate()
         pdf[key] = [x, y]
     # 'The data is in log scale (seconds).'
     np.savez_compressed(join(outputdir, 'pdf.npz'), o2o=pdf['o2o'], o2i=pdf['o2i'])
+
+    logger.info("Resampling data")
+    for key in ipt_dataset.keys():
+        data = ipt_dataset[key]
+        data[data == 0] = 1e-9
+        log_ipt = np.log10(data)
+        kernel_std = improved_sheather_jones(log_ipt.reshape(-1, 1))  # Shape (obs, dims)
+        # (1) First resample original data, then (2) add noise from kernel
+        resampled_data = np.random.choice(log_ipt, size=args.n, replace=True)
+        resampled_data = resampled_data + np.random.randn(args.n) * kernel_std
+        resampled_data = 10 ** resampled_data
+        with open(join(outputdir, '{}.txt'.format(key)), 'w') as f:
+            for data in resampled_data:
+                f.write("{:.6f}\n".format(data))
+
