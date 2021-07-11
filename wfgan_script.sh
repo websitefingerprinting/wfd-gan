66ddf7515c1d76e2e4c81205154e2fda5e92d034
Jiajun GONG <jgongac@connect.ust.hk>
2021-07-11 12:58:11 +0800

    a typo

--- wfgan_script.sh
+++ wfgan_script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 data_path=/ssd/jgongac/AlexaCrawler/parsed/dataset0_100_0711_103041_filtered/
 model_path=./dump/rimmer_top877_2000/training_0629_010626/model/
-o2o_path=./dump/rimmer_top877_2000/feature/feature/time_feature_0-100x0-1000_o2o.ipt
+o2o_path=./dump/rimmer_top877_2000/feature/time_feature_0-100x0-1000_o2o.ipt
 
 
 for i in $(seq 0.0 0.1 1)
